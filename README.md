1eca75cc3cee0fac939be3bd84bddb53a5415758
Pranjal <72604669+pran-jal@users.noreply.github.com>
2022-08-26 18:33:22 +0530

    Initial

---
+++ README.md
@@ -0,0 +1,11 @@
+# Reddit Image and Video Downloader
+
+Downloades all the images and videos from subreddits.
+
+- subreddits are mentioned in `sub_list.csv` file.
+- can adjust number of posts to get from each subreddit `( default = 20 )` .
+- Uses Threading for improved speed.
+- ffmpeg for downloading videos
+- subreddit posts can be sorted by `new`, `hot`, `top`, `gilded`, and `controversial`
+- skip already downloaded video `( defaults to override )`.
+- 
---
+++ main.py
@@ -0,0 +1,100 @@
+import praw
+import threading
+import requests
+import subprocess
+import os
+
+from config import *
+
+
+POST_SEARCH_AMOUNT = 20
+by  = ["new", "hot", "top", "gilded", "controversial"]
+header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:103.0) Gecko/20100101 Firefox/103.0"}
+dir_path = os.path.dirname(os.path.realpath(__file__))
+image_path = os.path.join(dir_path, "images/")
+CHECK_FOLDER = os.path.isdir(image_path)
+if not CHECK_FOLDER:
+    os.makedirs(image_path)
+video_path = os.path.join(dir_path, "video/")
+CHECK_FOLDER = os.path.isdir(video_path)
+if not CHECK_FOLDER:
+    os.makedirs(video_path)
+reddit = praw.Reddit(client_id=client_id, client_secret=client_secret, user_agent=user_agent,)
+subs = open("sub_list.csv", "r")
+
+
+def sort_by(subreddit, by = None):
+    if by:
+        return exec(f"{subreddit}.{by}(limit={POST_SEARCH_AMOUNT})")
+    return subreddit.new(limit=POST_SEARCH_AMOUNT)
+
+def download(url, name):
+    if os.path.exists(video_path+name+".mp4"):
+        # ans = input(f"File '{name}.mp4' already exists. Overwrite? [y/N] ")
+        ans = 'y'
+        if ans in ['N', 'n']:
+            return
+        os.remove(video_path+name+".mp4")
+    cmd = 'ffmpeg -i "%s" -c copy %s.mp4' %(url, name)
+    download = subprocess.run(cmd, cwd=video_path, capture_output=True).returncode
+    if download == 0:
+        return (f"{name} downloaded successfully")
+    else:
+        return (f"downloading {name} failed")
+
+
+
+def get_from(post, sub):
+    # for image
+    if post.url.lower().endswith(".jpg") or post.url.lower().endswith(".png"):
+        try:
+            resp = requests.get(post.url, headers=header).content
+            with open(f"{image_path}{sub}-{post.id}.png", "wb") as f:
+                f.write(resp)
+                f.close()
+                
+        except Exception as e:
+            print(f"failed. {post.url.lower()}")
+            print(e)
+
+    # for video
+    else:
+        if post.is_video:
+            m3u8 = post.media['reddit_video']['hls_url']
+            download(m3u8, f"{sub}-{post.id}")
+        else:
+            try:
+                resp = requests.head("https://www.reddit.com/video/" + post.url.split('/v.redd.it/')[1], headers=header).headers['Location']
+                json_url = resp[:-1:]+".json"
+                json_data = requests.get(json_url, headers=header).json()
+                m3u8 = json_data[0]["data"]["children"][0]["data"]["media"]["reddit_video"]["hls_url"]
+                download(m3u8, f"{sub}-{post.id}")
+            except Exception as e:
+                print(f"failed. {post.url.lower()}")
+                print(e)
+
+
+
+def main(by=None):
+    for line in subs:
+        sub = line.strip()
+        subreddit = sort_by(reddit.subreddit(sub), by=by)
+        print(f"Starting {sub}!")
+
+        threads = []
+
+        for post in subreddit:
+            t = threading.Thread(target = get_from, args=(post, sub,))
+            t.start()
+            threads.append(t)
+
+        for t in threads:
+            t.join()
+        for t in threads:
+            while t.is_alive():
+                continue
+        print(f"{sub} complete")
+
+if __name__ == "__main__":
+    main()
+
